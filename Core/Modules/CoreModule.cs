1cf224ebd9e6a305ea52b03ad6550d4609c84a2d
The Milk Man <ajayseanx@gmail.com>
2022-06-01 18:06:24 +0100

    add logging through DI

--- Core/Modules/CoreModule.cs
+++ Core/Modules/CoreModule.cs
@@ -1,4 +1,7 @@
+using Autofac.Extensions.DependencyInjection;
 using Core.Api;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace Core.Modules;
 
@@ -6,8 +9,16 @@ public class CoreModule : Module
 {
     protected override void Load(ContainerBuilder builder)
     {
+        builder.Populate(GetServices());
         builder.RegisterAssemblyTypes(System.Reflection.Assembly.GetExecutingAssembly())
             .Where(t => t.IsAssignableTo(typeof(Database)))
             .As<Database>();
+    }
+
+    private ServiceCollection GetServices()
+    {
+        var serviceCollection = new ServiceCollection();
+        serviceCollection.AddLogging(builder => builder.AddConsole());
+        return serviceCollection;
     }
 }
--- Core/Program.cs
+++ Core/Program.cs
@@ -1,6 +1,21 @@
+using Core.Modules;
+using Microsoft.Extensions.Logging;
+
 namespace Core;
 
 public class Program
 {
-    public static void Main(string[] args) => Console.WriteLine("Good morning world!");
+    public static void Main(string[] args)
+    {
+        using var services = Build();
+        var logger = services.Resolve<ILogger<Program>>();
+        logger.LogInformation("Good morning world!");
+    }
+
+    private static IContainer Build()
+    {
+        var builder = new ContainerBuilder();
+        builder.RegisterModule<CoreModule>();
+        return builder.Build();
+    }
 }
